c7adfdbd408dc07e3ad4108c8ec5a1d87aa30183
Tishanty <Tishantypetronella25@gmail.com>
2024-12-05 11:36:36 +0100

    css (tishanty)

--- resources/views/layout/main_layout.blade.php
+++ resources/views/layout/main_layout.blade.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,42 +23,66 @@
             </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-    @guest
-        {{-- Voor niet-ingelogde gebruikers --}}
-        <li>
-            <a href="{{ route('home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Home</a>
-        </li>
-        <li>
-            <a href="{{ route('login') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Log In</a>
-        </li>
-        <li>
-            <a href="{{ route('register') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Register</a>
-        </li>
-    @endguest
+            <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                @guest
+                    {{-- Voor niet-ingelogde gebruikers --}}
+                    <li>
+                        <a href="{{ route('home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Home</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('login') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Log In</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('register') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Register</a>
+                    </li>
+                @endguest
 
-    @auth
-        {{-- Voor ingelogde gebruikers --}}
-        @if(auth()->user()->is_admin)
-            <li>
-                <a href="{{ route('admin.home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Admin Home</a>
-            </li>
-        @else
-            <li>
-                <a href="{{ route('player.home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Player Home</a>
-            </li>
-        @endif
-        <li>
-            <a href="{{ route('logout') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white"
-               onclick="event.preventDefault(); document.getElementById('logout_form').submit();">
-                Log Out
-            </a>
-            <form id="logout_form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-        </li>
-    @endauth
-</ul>
+                @auth
+                    {{-- Voor ingelogde admin(Gaby) --}}
+                    @if(auth()->user()->is_admin)
+                        <li>
+                            <a href="{{ route('admin.home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Admin Home</a>
+                        <li>
+                            <a href="{{ url('account_edit') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Account Edit</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('player') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Player</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('score') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Score</a>
+                        </li>
+
+                        </li>
+                    @else  {{-- Voor ingelogde gebruikers --}}
+                    <li>
+                        <a href="{{ route('player.home') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">Player Home</a>
+                    <li class="relative group">
+                        <button class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
+                            Account
+                        </button>
+                        <ul class="absolute hidden mt-2 w-40 bg-gray-100 rounded shadow-lg p-2 dark:bg-gray-700 group-hover:block">
+                            <li>
+                                <a href="{{ route('user.edit') }}" class="block py-2 px-3 text-slate-400 rounded hover:bg-gray-200 dark:text-slate-400 dark:hover:bg-gray-600">Edit</a>
+                            </li>
+                            <li>
+                                <a href="{{ url('status') }}" class="block py-2 px-3 text-slate-400 rounded hover:bg-gray-200 dark:text-slate-400 dark:hover:bg-gray-600">Status</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    </li>
+                    @endif
+                    <li>
+                        <a href="{{ route('logout') }}" class="block py-2 px-3 md:p-0 text-slate-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white"
+                           onclick="event.preventDefault(); document.getElementById('logout_form').submit();">
+                            Log Out
+                        </a>
+                        <form id="logout_form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endauth
+            </ul>
 
         </div>
     </div>
@@ -69,7 +96,7 @@
 
 
 
-<footer class="bg`-slate-800 rounded-lg shadow m-4 dark:bg-gray-800">
+<footer class="bg-slate-800 rounded-lg shadow m-4 dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-200 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
