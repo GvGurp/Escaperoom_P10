d7fd612144df26e7c2462c7945fcf4b13ad1b8a0
Gaby <137078206+GvGurp@users.noreply.github.com>
2024-12-19 11:27:52 +0100

    User update
    user migration update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('firstname')->default('');
             $table->string('lastname')->nullable(); // Maak nullable
+            $table->string('username')->default('');
             $table->string('phonenumber');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
